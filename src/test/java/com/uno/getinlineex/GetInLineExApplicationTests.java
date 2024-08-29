f64aac3c601db2f81594e8e2dab1b53e5937fda0
sungink <iamsungink@gmail.com>
2024-08-29 14:37:02 +0900

    #1-setup

---
+++ src/test/java/com/uno/getinlineex/GetInLineExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uno.getinlineex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInLineExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
