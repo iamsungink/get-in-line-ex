d3ac36f892e964518750f1e217d81d48992dd112
sungink <iamsungink@gmail.com>
2024-08-30 10:37:37 +0900

    #4-thymeleaf 구현 및 error 통제

--- src/main/java/com/uno/getinlineex/controller/BaseController.java
+++ src/main/java/com/uno/getinlineex/controller/BaseController.java
@@ -1,14 +1,21 @@
 package com.uno.getinlineex.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController {
+public class BaseController implements ErrorController {
 
     @GetMapping("/")
     public String root() {
         System.out.println("root~~");
         return "index";
+    }
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error";
     }
 }
