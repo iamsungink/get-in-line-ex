57d8907f3c5947cf5566ca42f8c6e2ccd3bfa1b5
sungink <iamsungink@gmail.com>
2024-08-30 11:21:44 +0900

    #9-api

---
+++ src/main/java/com/uno/getinlineex/constant/EventStatus.java
@@ -0,0 +1,5 @@
+package com.uno.getinlineex.constant;
+
+public enum EventStatus {
+    PENDING, OPENED, CLOSED, CANCELLED, ABORTED
+}
---
+++ src/main/java/com/uno/getinlineex/dto/PlaceRequest.java
@@ -0,0 +1,23 @@
+package com.uno.getinlineex.dto;
+
+import com.uno.getinlineex.constant.PlaceType;
+
+public record PlaceRequest(
+        PlaceType placeType,
+        String placeName,
+        String address,
+        String phoneNumber,
+        Integer capacity,
+        String memo
+) {
+    public static PlaceRequest of(
+            PlaceType placeType,
+            String placeName,
+            String address,
+            String phoneNumber,
+            Integer capacity,
+            String memo
+    ) {
+        return new PlaceRequest(placeType, placeName, address, phoneNumber, capacity, memo);
+    }
+}
