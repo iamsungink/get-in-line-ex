9a300b5d4070ffa764c355aa61a0ac034ae3a8c3
sungink <iamsungink@gmail.com>
2024-09-02 16:27:31 +0900

    #13-JUnit 신규 기능 적용 (생성자 의존성 주입)

--- src/test/java/com/uno/getinlineex/controller/BaseControllerTest.java
+++ src/test/java/com/uno/getinlineex/controller/BaseControllerTest.java
@@ -3,64 +3,36 @@ package com.uno.getinlineex.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(BaseController.class)
 class BaseControllerTest {
 
-//    @BeforeEach
-//    void setUp() {
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//    }
+    private final MockMvc mvc;
 
-    @Autowired
-    private MockMvc mockMvc;
+    public BaseControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
 
-    @DisplayName("[view][GET] 기본페이지 요청")
+    @DisplayName("[view][GET] 기본 페이지 요청")
     @Test
-    void root() throws Exception{
+    void givenNothing_whenRequestingRootPage_thenReturnsIndexPage() throws Exception {
+        // Given
 
-        /*
-        //--------------#1
-
-        //Given
-
-        //When & Then
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(content().string(containsString("This is default page.")))
-                .andExpect(view().name("index"))
-                .andDo(print());
-         */
-
-        //--------------#2
-
-        //Given
-
-        //Wen
-        ResultActions resultActions = mockMvc.perform(get("/"));
-
-
-        //Then
-        resultActions
+        // When & Then
+        mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(content().string(containsString("This is default page.")))
                 .andExpect(view().name("index"))
                 .andDo(print());
     }
+
 }
