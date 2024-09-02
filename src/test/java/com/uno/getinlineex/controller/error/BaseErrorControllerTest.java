42ced33f91f7ed4be9ac004af4ffb6920c538539
sungink <iamsungink@gmail.com>
2024-09-02 17:20:55 +0900

    #13-API 테스트

---
+++ src/test/java/com/uno/getinlineex/controller/error/BaseErrorControllerTest.java
@@ -0,0 +1,49 @@
+package com.uno.getinlineex.controller.error;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BaseErrorController.class)
+class BaseErrorControllerTest {
+
+    private final MockMvc mvc;
+
+    public BaseErrorControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    //    @BeforeEach
+//    void setUp() {
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    @Test
+//    void errorHtml() {
+//    }
+
+    @DisplayName("[view][GET] 페이지없음")
+    @Test
+    void givenWrongURI_whenRequestingPage_thenReturns404ErrorPage() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/wrong-url"))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+}
